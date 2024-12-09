72cc5430d56e28ef858dcc0454ee9a4a3fa69ac6
chudgins-ftc <chudgins@philasd.org>
2024-12-09 15:57:45 -0500

    Update Arm_Test.java
    Added limits

--- Arm_Test.java
+++ Arm_Test.java
@@ -12,24 +12,24 @@ public class Arm_Test extends LinearOpMode {
 
     private ElapsedTime runtime = new ElapsedTime();
 
-	//Declare Motors and Servos
+    //Declare Motors and Servos
     public DcMotor BL_Motor = null;
     public DcMotor BR_Motor = null;
     public DcMotor FL_Motor = null;
     public DcMotor FR_Motor = null;
 
-	public DcMotor Rack_Motor = null;
-	public double Rack_Power = 0.0;
-	public double Rack_Pos = 0.0;
-	public double Rack_Upper = 1150.0;
-	public double Rack_Lower = 0.0;
-	public double Rack_Pad = 300.0;
+    public DcMotor Rack_Motor = null;
+    public double Rack_Power = 0.0;
+    public double Rack_Pos = 0.0;
+    public double Rack_Upper = 1150.0;
+    public double Rack_Lower = 0.0;
+    public double Rack_Pad = 300.0;
 
-	public DcMotor Arm_Motor = null;
-	public double Arm_Pos = 0.0;
-	public double Arm_Upper = 0.0;
-	public double Arm_Lower = 0.0;
-	public double Arm_Pad = 0.0;
+    public DcMotor Arm_Motor = null;
+    public double Arm_Pos = 0.0;
+    public double Arm_Upper = 0.0;
+    public double Arm_Lower = 0.0;
+    public double Arm_Pad = 0.0;
 
     public Servo Hand_Servo = null;
 
@@ -40,34 +40,34 @@ public class Arm_Test extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-	// Map Motors and Servos
+    // Map Motors and Servos
         BR_Motor = hardwareMap.get(DcMotor.class, "BR Wheel Motor");
         FL_Motor = hardwareMap.get(DcMotor.class, "FL Wheel Motor");
         BL_Motor = hardwareMap.get(DcMotor.class, "BL Wheel Motor");
         FR_Motor = hardwareMap.get(DcMotor.class, "FR Wheel Motor");
 
-		Rack_Motor = hardwareMap.get(DcMotor.class, "Rack Motor");
-		Rack_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
-		Rack_Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); 
-		
-	    Arm_Motor = hardwareMap.get(DcMotor.class, "Arm Motor");
-		Arm_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
-		Arm_Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); 
+        Rack_Motor = hardwareMap.get(DcMotor.class, "Rack Motor");
+        Rack_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        Rack_Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); 
+        
+        Arm_Motor = hardwareMap.get(DcMotor.class, "Arm Motor");
+        Arm_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        Arm_Motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); 
 
         Hand_Servo = hardwareMap.get(Servo.class, "Hand Servo");
 
-		double max = 0.0;
+        double max = 0.0;
 
         double axial   = 0.0;
         double lateral = 0.0;
         double yaw     = 0.0;
 
-	double leftFrontPower  = axial + lateral + yaw;
+    double leftFrontPower  = axial + lateral + yaw;
         double rightFrontPower = axial - lateral - yaw;
         double leftBackPower   = axial - lateral + yaw;
         double rightBackPower  = axial + lateral - yaw;
 
-		//Drive Test
+        //Drive Test
         //FL_Motor.setDirection(DcMotor.Direction.REVERSE);
         //BL_Motor.setDirection(DcMotor.Direction.REVERSE);
         //FR_Motor.setDirection(DcMotor.Direction.FORWARD);
@@ -83,25 +83,25 @@ public class Arm_Test extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-			//Update Sensors
-			Rack_Pos = Rack_Motor.getCurrentPosition();
-			Arm_Pos = Arm_Motor.getCurrentPosition();
+            //Update Sensors
+            Rack_Pos = Rack_Motor.getCurrentPosition();
+            Arm_Pos = Arm_Motor.getCurrentPosition();
 
-			//Calculate Rack Power
-			Rack_Power = gamepad1.right_trigger;
-			if (Rack_Pos < Rack_Lower + Rack_Pad){
-				Rack_Power = 0.0;
-			}
+            //Calculate Rack Power
+            Rack_Power = gamepad1.right_trigger - gamepad1.left_trigger;
+            if ((Rack_Power >= 0.0) && (Rack_Pos >= Rack_Upper - Rack_Pad)){
+                Rack_Power = ((Rack_Upper - Rack_Pos)/Rack_Pad)*Rack_Power;
+            }
 
-			if (Rack_Pos > Rack_Upper - Rack_Pad){
-				Rack_Power = 0.0;
-			}
-			
-			//Calculate Drive Power
-        	axial   = -gamepad1.left_stick_y;
-        	lateral =  gamepad1.left_stick_x;
-        	yaw     =  gamepad1.right_stick_x;
-			
+            if ((Rack_Power <= 0.0) && (Rack_Pos <= Rack_Lower + Rack_Pad)){
+                Rack_Power = ((Rack_Pos - Rack_Lower)/Rack_Pad)*Rack_Power;
+            }
+            
+            //Calculate Drive Power
+            axial   = -gamepad1.left_stick_y;
+            lateral =  gamepad1.left_stick_x;
+            yaw     =  gamepad1.right_stick_x;
+            
             leftFrontPower  = axial + lateral + yaw;
             rightFrontPower = axial - lateral - yaw;
             leftBackPower   = axial - lateral + yaw;
@@ -126,18 +126,18 @@ public class Arm_Test extends LinearOpMode {
             BL_Motor.setPower(leftBackPower);
             BR_Motor.setPower(rightBackPower);
 
-			// Send power to rack motor
-			Rack_Motor.setPower(Rack_Power);
+            // Send power to rack motor
+            Rack_Motor.setPower(Rack_Power);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            telemetry.addData("Rack_Power", "%4.2f", Rack_Power);
             telemetry.addData("Rack_Pos", "%4.2f", Rack_Pos);
-			telemetry.addData("Arm_Pos", "%4.2f", Arm_Pos);
+            telemetry.addData("Arm_Pos", "%4.2f", Arm_Pos);
 
             telemetry.update();
         }
-	
+    
     }}
-
